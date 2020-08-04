746defad7cb6fda2e444c07da290d96fcc685af7
Yaye Esmilla <gam.esmilla@gmail.com>
2020-08-04 13:38:47 -0400

    removed analytics and updated manifest

--- js/popup.js
+++ js/popup.js
@@ -8,21 +8,6 @@ let CURRENT = null;
 let OPERAND = null;
 let KEYS = [];
 
-function getCurrentTabUrl(callback) {
-    var queryInfo = {
-            active: true,
-            currentWindow: true
-        };
-
-    chrome.tabs.query(queryInfo, (tabs) => {
-        var tab = tabs[0];
-        var url = tab.url;
-        console.assert(typeof url == 'string', 'tab.url should be a string');
-
-        callback(url, tab);
-    });
-}
-
 function buildElements() {
 
     let $row, $keyButton;
@@ -334,8 +319,6 @@ function getKeysValue() {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    getCurrentTabUrl((url, tab) => {
-        buildElements()
-            .then(bindEventListeners);
-    });
+    buildElements()
+        .then(bindEventListeners);
 });
